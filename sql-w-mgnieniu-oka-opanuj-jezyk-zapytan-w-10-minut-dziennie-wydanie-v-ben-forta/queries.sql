c289656d2fb3ef34c18d16644e7198b299bde62d
wyderka-j <131911420+wyderka-j@users.noreply.github.com>
2025-01-27 11:00:10 +0100

    Update queries.sql

--- sql-w-mgnieniu-oka-opanuj-jezyk-zapytan-w-10-minut-dziennie-wydanie-v-ben-forta/queries.sql
+++ sql-w-mgnieniu-oka-opanuj-jezyk-zapytan-w-10-minut-dziennie-wydanie-v-ben-forta/queries.sql
@@ -19,3 +19,25 @@ SELECT *
 -- SELECT kl_id
 FROM Klienci;
 
+-- ****************************************
+-- Rozdział 3 Sortowanie pobranych danych  
+-- ****************************************
+-- 1. Napisz SQL-u instrukcję, która pobiera nazwy wszystkich klientów (kl_nazwa) z tabeli Klienci. Wyświetl wyniki posortowane od Z do A.
+SELECT kl_nazwa
+FROM Klienci
+ORDER BY kl_nazwa DESC;
+-- 2. Napisz SQL-u instrukcję, która pobiera identyfikator klienta (kl_id) i numer zamówienia (zam_numer)  tabeli Zamowienia oraz sortuje wyniki 
+--  najpierw według identyfikatora klientów, a następnie według ddaty zamówienia w odwrotnej kolejności chronologicznej (czyli od najnowszych).
+SELECT kl_id, zam_numer
+FROM Zamowienia
+ORDER BY kl_id, zam_data DESC;
+-- 3. Napisz SQL-u instrukcję, która wyświetla liczbę sztuk i cenę jednostkową (prod_cena) x tabeli ElementyZamowienia. Wyniki mają być posortowane 
+-- od największej liczby sztuk i najwyższych cen.
+SELECT ilosc, prod_cena
+FROM ElementyZamowienia
+ORDER BY ilosc DESC, prod_cena DESC;
+--4. Jaki błąd znajduje się w poniższej instrukcji w SQL?
+     /*  SELECT dost)nazwa,
+         FROM Dostawcy
+         Order dost_nazwa DESC;  */
+-- Po nazwie dost_nazwa nie powinno być przecinka. Ponadto po ORDER brakuje słowa BY.
